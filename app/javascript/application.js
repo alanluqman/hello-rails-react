6ac568490be2e3010e425149439acd92d19df8e9
alanluqman <35001742+alanluqman@users.noreply.github.com>
2023-01-11 17:58:13 +0300

    Update react interface

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,13 +3,12 @@ import "@hotwired/turbo-rails"
 import "./controllers"
 
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-function App() {
-  return (<h1>Hello World!</h1>);
-}
-
-ReactDOM.render(
-  <App/>,
-  document.getElementById('root'),
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 );
